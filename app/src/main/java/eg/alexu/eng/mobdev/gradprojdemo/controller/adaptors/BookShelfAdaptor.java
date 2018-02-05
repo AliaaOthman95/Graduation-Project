d28993203cfa4ea0a904babf0fd49a9f73e450ac
PBBassily <pbbassily@gmail.com>
2018-02-05 17:48:48 +0200

    book sheld added

---
+++ app/src/main/java/eg/alexu/eng/mobdev/gradprojdemo/controller/adaptors/BookShelfAdaptor.java
@@ -0,0 +1,77 @@
+package eg.alexu.eng.mobdev.gradprojdemo.controller.adaptors;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import eg.alexu.eng.mobdev.gradprojdemo.R;
+import eg.alexu.eng.mobdev.gradprojdemo.model.Story;
+
+/**
+ * Created by Paula B. Bassily on 05/02/2018.
+ */
+
+    public class BookShelfAdaptor extends RecyclerView.Adapter<BookShelfAdaptor.StroyCardViewHolder>{
+
+        List<Story> storyList ;
+
+        public BookShelfAdaptor(List<Story> stories){
+            this.storyList=stories;
+        }
+
+        @Override
+        public StroyCardViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.story_crad_view, parent, false);
+            StroyCardViewHolder stroyCardViewHolder = new StroyCardViewHolder(view);
+            return stroyCardViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(StroyCardViewHolder holder, int index) {
+
+            holder.storyName.setText(storyList.get(index).getStroyName());
+
+            holder.storyDate.setText(storyList.get(index).getCreationDate()+"");
+
+            Context context = holder.storyCover.getContext();
+            String coverName = storyList.get(index).getCover();
+            int coverImageId = context.getResources().getIdentifier(coverName,
+                    "drawable", context.getPackageName());
+            holder.storyCover.setImageResource(coverImageId);
+
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return storyList.size();
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        public static class StroyCardViewHolder extends RecyclerView.ViewHolder {
+            CardView cv;
+            TextView storyName;
+            TextView storyDate;
+            ImageView storyCover;
+
+            StroyCardViewHolder(View itemView) {
+                super(itemView);
+                cv = (CardView)itemView.findViewById(R.id.cv);
+                storyName = (TextView)itemView.findViewById(R.id.story_name);
+                storyDate = (TextView)itemView.findViewById(R.id.story_date);
+                storyCover = (ImageView)itemView.findViewById(R.id.story_cover);
+            }
+        }
+}
--- app/src/main/java/eg/alexu/eng/mobdev/gradprojdemo/model/Story.java
+++ app/src/main/java/eg/alexu/eng/mobdev/gradprojdemo/model/Story.java
@@ -12,9 +12,60 @@ import java.util.List;
 public class Story {
 
     private String stroyName;
-    private Image cover ;
+    private String cover ;
     private Color coverColor;
     private Date creationDate ;
     private List<Scene> scenes;
+
+    public Story(String stroyName, Date creationDate) {
+        this.stroyName = stroyName;
+        this.creationDate = creationDate;
+    }
+
+    public Story(String stroyName, Date creationDate, String cover) {
+        this.stroyName = stroyName;
+        this.creationDate = creationDate;
+        this.cover = cover;
+    }
+
+    public String getStroyName() {
+        return stroyName;
+    }
+
+    public void setStroyName(String stroyName) {
+        this.stroyName = stroyName;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public Color getCoverColor() {
+        return coverColor;
+    }
+
+    public void setCoverColor(Color coverColor) {
+        this.coverColor = coverColor;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public List<Scene> getScenes() {
+        return scenes;
+    }
+
+    public void setScenes(List<Scene> scenes) {
+        this.scenes = scenes;
+    }
 
 }
